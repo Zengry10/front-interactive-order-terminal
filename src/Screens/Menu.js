e3367908a33d06bd19cadbfd8ed9bc8831466f3e
Zengry10 <remi.lemoust-de-lafosse@epitech.digital>
2023-01-31 14:43:27 +0100

    add style and display menu

--- src/Screens/Menu.js
+++ src/Screens/Menu.js
@@ -17,18 +17,18 @@ export default function Menu(){
     }, [])
         if (articles) {
             return(
-                <div>
-                    <h1> ca marche{articles[0].description}</h1>
-                    {articles.map((article) => {
-                        return(
-                            <div key={article.id}>
-                                <h1>{article.name}</h1>
-                               
-                                <img src={article.picture} alt=""/>
-                            </div>
-                        )
-                    }
-                    )}
+                <div className="flex flex-wrap items-center bg-orange-500 p-4 justify-center">
+                {articles.map((article) => {
+                    return(
+                    <div key={article.id} className="w-full md:w-1/3 max-w-sm m-2 bg-white rounded-lg shadow-md">
+                        <img className="w-full" src={article.picture} alt="menu item"/>
+                        <div className="p-4 flex items-center justify-between">
+                        <h3 className="text-xl font-bold">{article.name}</h3>
+                        <p className="bg-red-600 text-white px-4 py-2 rounded-full">{article.price}€</p>
+                        </div>
+                    </div>
+                    )
+                })}
                 </div>
             )
         }
